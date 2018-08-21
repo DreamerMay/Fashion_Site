7edeb18e2e3e3d28a9f1719eeb72a7385a631302
Sherine <sherinemay79@gmail.com>
2018-08-21 10:15:06 +1000

    trying to revert

--- config/routes.rb
+++ config/routes.rb
@@ -4,21 +4,21 @@ Rails.application.routes.draw do
   root :to => 'pages#home'
   get "/" => 'pages#home'
 
-  resources :styles, :only => [:new, :by_type, :show] do
+  resources :styles
     get '/new' => 'styles#new'
     get '/by_type' => 'styles#by_type', as: :by_type
     get '/show/:name' =>'style#show', as: :style_show
-  end
 
 
 
-  resources :attires, :only => [:new, :by_type, :show] do
+
+  resources :attires
     get '/new' => 'attires#new'
     get '/by_type/:attire' => 'attires#by_type', as: :by_type
     get '/show/:name' => 'attire#show', as: :attire_show
-  end
 
-  resources :users, :only => [:new, :create, :index, :fav_attires, :fav_styles]
+
+  resources :users
     get '/new' => 'users#new'
     get '/login' => 'session#new'
     post '/login' => 'session#create'
